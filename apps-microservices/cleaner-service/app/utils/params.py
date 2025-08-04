fd8b4cc4356a2ac76b3c115739cf911b3700ac74
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2025-08-04 14:12:47 +0300

    feat(maj): Suppression import embedding

--- apps-microservices/cleaner-service/app/utils/params.py
+++ apps-microservices/cleaner-service/app/utils/params.py
@@ -1,7 +1,6 @@
 from app.utils.router.tags import RouterTags as Tags
 
 from app.router.cleaner.cleaner import router as CleanerRouter
-from app.router.embedding.embedding import router as EmbeddingRouter
 
 params = [
     [
