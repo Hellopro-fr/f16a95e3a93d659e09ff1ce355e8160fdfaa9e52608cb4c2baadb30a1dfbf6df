546149595f29f35cae927f4bde8089c4403ba9bb
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-08-05 10:29:55 +0300

    feat(maj): ajout de modèle de retour

--- apps-microservices/cleaner-service/app/schemas/cleaner/cleaner.py
+++ apps-microservices/cleaner-service/app/schemas/cleaner/cleaner.py
@@ -7,8 +7,15 @@ from app.schemas.base import GetBase as Base
 class BaseCleaner(BaseModel):
     url: Annotated[str, Field(title="L'url du site")]
     content: Annotated[str, Field(title="Le contenu du site")]
-    fetch_content: Annotated[bool, Field(title="Récupérer le contenu du site en utilisant fetch fonction de trafilatura ")] = False
+    fetch: Annotated[bool, Field(title="Récupérer le contenu du site en utilisant fetch fonction de trafilatura ")] = False
 
+
+class BaseCleanerReponse(BaseModel):
+    url: Annotated[str, Field(title="L'url du site")]
+    content: Annotated[str, Field(title="Le contenu du site")]
+
+class ReponseHtml(BaseCleanerReponse):
+    html: Annotated[str, Field(title="Contenu html du site si fetch contenu")]
 
 class GetBase(BaseCleaner):
     uid: uuid.UUID
